5f11314bf0e74d56a3ec895f4420ec7e9b6d39ee
aeryskyB <79935164+aeryskyB@users.noreply.github.com>
2025-02-11 10:32:56 +0530

    fix wrong probability assignment for bernoulli dist.

--- dropout/dropout.py
+++ dropout/dropout.py
@@ -1,23 +1,27 @@
-import jax.numpy as jnp
 import jax
 from jax.typing import ArrayLike
 from typing import Union
 
-def dropout(y: ArrayLike, p: float = 0.5, key: Union[None, jax.Array] = None, is_training: bool = True) -> jax.Array:
-    assert p >= 0 and p <= 1, "p must be in the range [0, 1]"
+def dropout(y: ArrayLike, dropout_p: float = 0.5, key: Union[None, jax.Array] = None, is_training: bool = True) -> ArrayLike:
+    assert dropout_p >= 0 and dropout_p <= 1, "p must be in the range [0, 1]"
 
     if is_training:
         if key is None: key = jax.random.key(0)
-        r = jax.random.bernoulli(key=key, p=p, shape=y.shape)
+        r = jax.random.bernoulli(key=key, p=1-dropout_p, shape=y.shape)
         y_ = r * y
     else:
-        y_ = p * y
+        y_ = dropout_p * y
 
     return y_
 
 if __name__ == "__main__":
     key = jax.random.key(2024)
-    y = jax.random.normal(key=key, shape=(10,))
-    y_ = dropout(y, key=key)
-    print('y: ', y)
-    print('y_: ', y_)
+    n = 5
+    keys = jax.random.split(key, n+1)
+
+    y = jax.random.normal(key=keys[-1], shape=(10,))
+    print('y:\n', y)
+    for i in range(n):
+        y_ = dropout(y, dropout_p=0.1, key=keys[i])
+        print('\ny_:\n', y_)
+
