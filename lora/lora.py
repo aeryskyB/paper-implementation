41cbbc0d008d1443d49f8b43bc55686efbe778e8
aeryskyB <79935164+aeryskyB@users.noreply.github.com>
2025-02-16 13:34:04 +0530

    add test: adapter for a simple ffn

--- lora/lora.py
+++ lora/lora.py
@@ -5,12 +5,12 @@ from jax import random, Array
 
 
 class Linear:
-    def __init__(self, in_dim: int, out_dim: int, r: int, alpha: float = 1., key: Array = random.key(0), dtype=jnp.float32):
+    def __init__(self, in_dim: int, out_dim: int, r: int, alpha: float, key: Array = random.key(0), dtype=jnp.float32):
         self.A = random.normal(key=key, shape=(r, in_dim), dtype=dtype)
         self.B = jnp.zeros(shape=(out_dim, r), dtype=dtype)
         self.r = r
         self.alpha = alpha
-        self.scaling_factor = self.alpha / self.r
+        self.scaling_factor = jnp.asarray(self.alpha / self.r, dtype=jnp.float32)
 
     def __call__(self, x: Array):
         # returns the partial product BAx during the forward pass
--- lora/test_lora.py
+++ lora/test_lora.py
@@ -1,6 +1,7 @@
 import jax.numpy as jnp
-from jax import random, Array, vmap, grad, jit
+from jax import random, Array, vmap, grad, jit, debug
 from typing import List
+from time import monotonic
 
 from lora import Linear
 
@@ -19,12 +20,24 @@ def ffn(dims: List[int], key: Array = random.key(0)):
 def relu(x):
     return jnp.maximum(0, x)
 
-def ffn_fwd_with_lora(params, lora_param, x):
-    (lw, lb), params = params[-1], params[:-1]
+def ffn_fwd(params, x):
+    (fw, fb), params = params[-1], params[:-1]
     y = x
     for (w, b) in params:
         y = relu(w @ y + b)
-    y = lw @ y + lb + lora_param @ x
+    y = fw @ y + fb
+    return y
+
+batched_ffn_fwd = vmap(ffn_fwd, (None, 0), 0)
+
+def ffn_fwd_with_lora(params, lora_params, x):
+    (fw, fb), params = params[-1], params[:-1]
+    (s, flb, fla), lora_params = lora_params[-1], lora_params[:-1]
+    y = x
+    for (w, b), (s, lb, la) in zip(params, lora_params):
+        y = relu((w + s*lb@la) @ y + b)
+    y = (fw + s*flb@fla) @ y + fb
+    # debug.print("👀{x}", x=y.shape)
     return y
 
 batched_ffn_fwd_with_lora = vmap(ffn_fwd_with_lora, (None, None, 0), 0)
@@ -32,21 +45,37 @@ batched_ffn_fwd_with_lora = vmap(ffn_fwd_with_lora, (None, None, 0), 0)
 def mse_loss(y_pred, y):
     return jnp.sum((y - y_pred)**2 / y.shape[0])
 
-def predict(params, lora_param, x, y):
-    y_pred = batched_ffn_fwd_with_lora(params, lora_param, x)
+def predict(params, x, y):
+    y_pred = batched_ffn_fwd(params, x)
+    return mse_loss(y_pred, y)
+
+def predict_lora(params, lora_params, x, y):
+    y_pred = batched_ffn_fwd_with_lora(params, lora_params, x)
+    # debug.print("👀{x}", x=y_pred.shape)
     return mse_loss(y_pred, y)
 
 @jit
-def update_lora(params, lora_param, x, y, lr):
-    lora_grad = grad(predict, 1)(params, lora_param, x, y)
-    return lora_param - lr*lora_grad
+def update(params, x, y, lr):
+    grads = grad(predict, 0)(params, x, y)
+    return [(w - lr*dw, b - lr*db) for (w, b), (dw, db) in zip(params, grads)]
+
+@jit
+def update_lora(params, lora_params, x, y, lr):
+    # debug.print("🥲{x}", x=lora_params[0])
+    lora_grads = grad(predict_lora, 1)(params, lora_params, x, y)
+    # debug.print("😭{x}", x=lora_grads[0])
+    ## don't tune scaling factor
+    updated_lora_params = [(s, b-lr*db, a-lr*da) for (s, b, a), (_, db, da) in zip(lora_params, lora_grads)]
+    # debug.print("zzzzzzzz {x}", x=updated_lora_params[0][1].shape)
+    # debug.print("kkkkkkkk {x}", x=lora_params[0][1].shape)
+    return updated_lora_params
 
 if __name__ == "__main__":
     keys = random.split(random.key(7), 2)
-    lr = 2e-4
-    batch_size = 64
-    num = 1_000
-    epoch = 5
+    lr = 2e-3
+    batch_size = 128
+    num = 10_000
+    epoch = 10
 
     x = random.normal(key=keys[0], shape=(num, 1024))
     y = random.uniform(key=keys[1], shape=(num, 10))
@@ -60,20 +89,50 @@ if __name__ == "__main__":
     y_test = y[train_num:]
 
     in_dim, out_dim = (1024, 10)
-    lora_test = Linear(in_dim=in_dim, out_dim=out_dim, r=4, key=keys[0])
-    ba = lora_test()
 
-    # rather some pretrained weights, 
+    # rather some pretrained weights
     dims = [in_dim] + [512, 128] + [out_dim]
     ffn_params = ffn(dims, key=keys[1])
 
+    lora_keys = random.split(keys[0], len(dims)-1)
+    lora_r = 4
+    lora_alpha = lora_r
+    loras = [Linear(in_dim=i_d, out_dim=o_d, r=lora_r, alpha=lora_alpha, key=k) \
+        for i_d, o_d, k in zip(dims[:-1], dims[1:], lora_keys)]
+
+    lora_params = [(l.scaling_factor, l.B, l.A) for l in loras]
+
+
+    print("<-train the base ffn->")
+    t = 0
     for e in range(epoch):
+        t0 = monotonic()
         for i in range(0, num, batch_size):
-            ba = update_lora(ffn_params, ba,
-                             x_train[i*batch_size:(i+1)*batch_size],
-                             y_train[i*batch_size:(i+1)*batch_size],
-                             lr=lr)
-        train_loss = predict(ffn_params, ba, x_train, y_train)
-        test_loss = predict(ffn_params, ba, x_test, y_test)
-        print(f"epoch {e} | {train_loss=} | {test_loss=}")
+            ffn_params = update(ffn_params,
+                                x_train[i:i+batch_size],
+                                y_train[i:i+batch_size],
+                                lr=lr)
+        dt = monotonic() - t0
+        t += dt
+        train_loss = predict(ffn_params, x_train, y_train)
+        test_loss = predict(ffn_params, x_test, y_test)
+        print(f"epoch {e:>2} | {train_loss=:<20} | {test_loss=}")
+    print(f"time taken: {t}\n")
+
+    lora_lr = 8e-4
+    print("<-train lora->")
+    t = 0
+    for e in range(epoch):
+        t0 = monotonic()
+        for i in range(0, train_num, batch_size):
+            lora_params = update_lora(ffn_params, lora_params,
+                                      x_train[i:i+batch_size],
+                                      y_train[i:i+batch_size],
+                                      lr=lora_lr)
+        dt = monotonic() - t0
+        t += dt
+        train_loss = predict_lora(ffn_params, lora_params, x_train, y_train)
+        test_loss = predict_lora(ffn_params, lora_params, x_test, y_test)
+        print(f"epoch {e:>2} | {train_loss=:<20} | {test_loss=}")
+    print(f"time taken: {t}")
 
